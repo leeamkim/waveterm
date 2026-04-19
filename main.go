6aeffba7b5f73b0e54a7d638d8c9085cfeea3f89
Lee Kim <leeamkim@users.noreply.github.com>
2026-04-20 00:11:56 +0800

    feat: add main entry point

---
+++ main.go
@@ -0,0 +1,29 @@
+// Copyright 2025, Command Line Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/wavetermdev/waveterm/pkg/wavebase"
+)
+
+var WaveVersion = "v0.0.1"
+var BuildTime = "0"
+
+func main() {
+	wavebase.WaveVersion = WaveVersion
+	wavebase.BuildTime = BuildTime
+
+	if err := runMain(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func runMain() error {
+	fmt.Printf("WaveTerm %s (build %s)\n", WaveVersion, BuildTime)
+	return nil
+}
